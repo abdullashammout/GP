ff35f5623e1ecc245e735d21ec55e5cebef40469
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-06 01:23:04 +0300

    i added home page and some styles on it

--- styles/homeStyle.js
+++ styles/homeStyle.js
@@ -2,9 +2,35 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
+  },
+  Logo: {
+    position: "absolute",
+    top: 30,
+  },
+  patient: {
+    width: 280,
+    borderColor: "white",
+    padding: 10,
+    backgroundColor: "#4169E1",
+    borderWidth: 1,
+    borderRadius: 10,
+    shadowRadius: 10,
+    position: "absolute",
+    bottom: 160,
+  },
+  MedicalUnit: {
+    width: 280,
+    borderColor: "white",
+    padding: 10,
+    backgroundColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    shadowRadius: 10,
+    position: "absolute",
+    bottom: 100,
   },
 });
 export default styles;
