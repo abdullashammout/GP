b9f03e68c3d0d25a1d8cd8b49ccdba58835e6c39
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-06 02:51:40 +0300

    i added a new screen named patientHome

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./screens/home";
+import PaitentHome from "./screens/patient/home";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,13 @@ export default function App() {
         <Stack.Screen
           name="home"
           component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="patientHome"
+          component={PaitentHome}
           options={{
             headerShown: false,
           }}
