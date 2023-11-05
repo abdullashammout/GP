090f59be481947ee3f8e2e9af6c972b7903da548
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-05 17:00:53 +0300

    2 files add home and homeStyle

---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import styles from "../styles/homeStyle";
+
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>hii iam home</Text>
+    </View>
+  );
+}
