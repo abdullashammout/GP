d466a9c5e65b20d6978b06ee344651744c719b86
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-15 05:21:53 +0300

    nothing

--- App.js
+++ App.js
@@ -81,7 +81,7 @@ export default function App() {
           options={{
             headerShadowVisible: false,
             headerTitleAlign: "center",
-            title: "phar",
+            title: "pharm",
           }}
         />
         <Stack.Screen
