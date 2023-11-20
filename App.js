8f5abff82f08afbd9b4cedb72a237c2fd49851d8
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-20 20:35:39 +0300

    handle login

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { auth } from "./firebase";
+import { Alert, BackHandler } from "react-native";
+
 import Home from "./screens/home";
 import PaitentHome from "./screens/patient/home";
 import LoginForm from "./screens/patient/login";
@@ -27,19 +29,69 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const navigationRef = useRef();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      // onAuthStateChanged is called when the user's login status changes
-      setUserLoggedIn(!!user); // Update the userLoggedIn state based on the user's login status
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setUserLoggedIn(!!user);
+
+      if (!user) {
+        navigationRef.current.navigate("home");
+      }
     });
 
-    // Cleanup function to unsubscribe the listener when the component unmounts
-    return () => unsubscribe();
-  }, []);
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      () => {
+        const currentRoute = navigationRef.current.getCurrentRoute();
+
+        // Check if the user is on one of the special screens
+        if (
+          currentRoute.name === "hospital" ||
+          currentRoute.name === "pharmacy" ||
+          currentRoute.name === "laboratory" ||
+          currentRoute.name === "patientPage"
+        ) {
+          Alert.alert(
+            "Logout Confirmation",
+            "Are you sure you want to logout?",
+            [
+              {
+                text: "Cancel",
+                style: "cancel",
+              },
+              {
+                text: "Logout",
+                onPress: async () => {
+                  try {
+                    await auth.signOut(); // Sign out the user
+                  } catch (error) {
+                    console.error("Error during logout:", error.message);
+                    // Show an error alert if there is an issue during logout
+                    Alert.alert(
+                      "Logout Error",
+                      "An error occurred during logout. Please try again."
+                    );
+                  }
+                },
+              },
+            ]
+          ); // logout
+          return true; // Tell the app that we've handled the back press
+        }
+
+        return false; // Let the default back press behavior happen
+      }
+    );
+
+    return () => {
+      unsubscribe();
+      backHandler.remove(); // Remove the event listener when the component unmounts
+    };
+  }, [userLoggedIn]);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
         {userLoggedIn ? (
           <>
@@ -55,18 +107,14 @@ export default function App() {
               name="pharmacy"
               component={SearchPh}
               options={{
-                headerShadowVisible: false,
-                headerTitleAlign: "center",
-                title: "phar",
+                headerShown: false,
               }}
             />
             <Stack.Screen
               name="laboratory"
               component={SearchLabor}
               options={{
-                headerShadowVisible: false,
-                headerTitleAlign: "center",
-                title: "lab",
+                headerShown: false,
               }}
             />
 
