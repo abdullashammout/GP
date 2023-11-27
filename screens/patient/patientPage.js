1be2db53e4a8280bf549e2d3dfb8bc373c7f31ac
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-27 05:47:36 +0300

    create a drawer and fetch patient name

--- screens/patient/patientPage.js
+++ screens/patient/patientPage.js
@@ -1,9 +1,69 @@
-import { View, Text, TouchableOpacity, Alert } from "react-native";
-import { auth } from "../../firebase";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+  Dimensions,
+  Animated,
+} from "react-native";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import styles from "../../styles/patientStyles/patientPageStyle";
+import { ref, get } from "firebase/database";
+import { db, auth } from "../../firebase";
 
-export default function PatientPage() {
+export default function PatientPage({ navigation, route }) {
+  const { userId } = route.params || {};
+  const [patientName, setPatientName] = useState("");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const [animation] = useState(new Animated.Value(0));
+  useEffect(() => {
+    const fetchHospitalName = async () => {
+      try {
+        const userRef = ref(db, `users/patients/${userId}`);
+        const snapshot = await get(userRef);
+
+        if (snapshot.exists()) {
+          const { name } = snapshot.val();
+          setPatientName(name);
+        }
+      } catch (error) {
+        console.error("Error fetching hospital data:", error);
+      } finally {
+        // Set loading to false when data is fetched or an error occurs
+      }
+    };
+
+    fetchHospitalName();
+  }, [userId]);
+
+  const openDrawer = () => {
+    setDrawerOpen(true);
+    Animated.timing(animation, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const closeDrawer = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start(() => {
+      setDrawerOpen(false);
+    });
+  };
+
+  const translateX = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -Dimensions.get("window").width * 0.6],
+  });
+
   const logout = async () => {
     Alert.alert("Logout Confirmation", "Are you sure you want to logout?", [
       {
@@ -28,15 +88,59 @@ export default function PatientPage() {
       },
     ]);
   };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>hiii iam patient page</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TouchableOpacity
+        style={{ position: "absolute", top: 60, left: 40 }}
+        onPress={openDrawer}
+      >
+        <Ionicons name="menu" size={24} color="black" />
+      </TouchableOpacity>
+
+      <Text>Hi, I am the patient page</Text>
       <TouchableOpacity
         style={{ position: "absolute", top: 60, right: 40 }}
         onPress={logout}
       >
         <MaterialCommunityIcons name="logout" size={24} color="black" />
       </TouchableOpacity>
+      {drawerOpen && (
+        <Animated.View style={[styles.drawer, { transform: [{ translateX }] }]}>
+          <ScrollView>
+            <TouchableOpacity style={styles.backButton} onPress={closeDrawer}>
+              <Ionicons name="arrow-back" size={24} color="white" />
+            </TouchableOpacity>
+            <View style={styles.drawerHeader}>
+              <Ionicons
+                name="md-person-circle-outline"
+                size={60}
+                color="white"
+              />
+              <Text style={styles.patientName}>{patientName}</Text>
+            </View>
+          </ScrollView>
+        </Animated.View>
+      )}
+      {drawerOpen && (
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "60%",
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0,0,0)",
+          }}
+          onPress={closeDrawer}
+        />
+      )}
     </View>
   );
 }
