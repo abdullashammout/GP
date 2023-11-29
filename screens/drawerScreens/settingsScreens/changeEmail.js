f46786c1e31f246d98352146c6f9e6732f1a1fe1
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-29 04:15:57 +0300

    drawer screens

---
+++ screens/drawerScreens/settingsScreens/changeEmail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ChangeEmailScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Change email</Text>
+    </View>
+  );
+}
