d7c1dc90bb23a262e7c3c4451cbf5fb761e587e9
abdullashammout <136109835+abdullashammout@users.noreply.github.com>
2023-11-29 04:17:45 +0300

    patient home page with styles

--- screens/patient/patientPage.js
+++ screens/patient/patientPage.js
@@ -7,8 +7,15 @@ import {
   ScrollView,
   Dimensions,
   Animated,
+  FlatList,
+  Image,
 } from "react-native";
-import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  SimpleLineIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "../../styles/patientStyles/patientPageStyle";
 import { ref, get } from "firebase/database";
@@ -18,27 +25,32 @@ export default function PatientPage({ navigation, route }) {
   const { userId } = route.params || {};
   const [patientName, setPatientName] = useState("");
   const [drawerOpen, setDrawerOpen] = useState(false);
-
   const [animation] = useState(new Animated.Value(0));
+
   useEffect(() => {
-    const fetchHospitalName = async () => {
+    const fetchPatientName = async () => {
       try {
         const userRef = ref(db, `users/patients/${userId}`);
         const snapshot = await get(userRef);
 
         if (snapshot.exists()) {
           const { name } = snapshot.val();
-          setPatientName(name);
+          await AsyncStorage.setItem("PatientName", name);
         }
       } catch (error) {
-        console.error("Error fetching hospital data:", error);
+        console.error("Error fetching Patient data:", error);
       } finally {
         // Set loading to false when data is fetched or an error occurs
       }
     };
 
-    fetchHospitalName();
+    fetchPatientName();
   }, [userId]);
+  const getPatientName = async () => {
+    const pName = await AsyncStorage.getItem("PatientName");
+    setPatientName(pName);
+  };
+  getPatientName();
 
   const openDrawer = () => {
     setDrawerOpen(true);
@@ -76,6 +88,7 @@ export default function PatientPage({ navigation, route }) {
           try {
             await auth.signOut(); // Sign out the user
             await AsyncStorage.removeItem("userRole");
+            await AsyncStorage.removeItem("PatientName");
           } catch (error) {
             console.error("Error during logout:", error.message);
             // Show an error alert if there is an issue during logout
@@ -88,6 +101,72 @@ export default function PatientPage({ navigation, route }) {
       },
     ]);
   };
+  const data = [
+    {
+      index: "1",
+      title: "Prescriptions",
+      image: require("../../pics/pres.png"),
+    },
+    {
+      index: "2",
+      title: "Diagnosis",
+      image: require("../../pics/diagnosis.png"),
+    },
+    { index: "3", title: "Treatments", image: require("../../pics/treat.png") },
+    { index: "4", title: "Vaccine", image: require("../../pics/vaccine.png") },
+    {
+      index: "5",
+      title: "Allergies",
+      image: require("../../pics/allergy.png"),
+    },
+    {
+      index: "6",
+      title: "Lab results",
+      image: require("../../pics/results.png"),
+    },
+    {
+      index: "7",
+      title: "Blood donation",
+      image: require("../../pics/blood.png"),
+    },
+    {
+      index: "8",
+      title: "Hospital stay",
+      image: require("../../pics/stay.png"),
+    },
+  ];
+  const toPresc = (index) => {
+    if (index === "1") {
+      navigation.navigate("Prescription");
+    }
+    if (index === "2") {
+      navigation.navigate("diagnosis");
+    }
+    if (index === "3") {
+      navigation.navigate("treatments");
+    }
+    if (index === "4") {
+      navigation.navigate("vaccine");
+    }
+    if (index === "5") {
+      navigation.navigate("allergies");
+    }
+    if (index === "6") {
+      navigation.navigate("lab");
+    }
+    if (index === "7") {
+      navigation.navigate("BloodDonation");
+    }
+    if (index === "8") {
+      navigation.navigate("hospitalStay");
+    }
+  };
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.item} onPress={() => toPresc(item.index)}>
+      <Image source={item.image} style={styles.image} />
+      <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View
@@ -95,22 +174,52 @@ export default function PatientPage({ navigation, route }) {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#ecf0f1",
       }}
     >
-      <TouchableOpacity
-        style={{ position: "absolute", top: 60, left: 40 }}
-        onPress={openDrawer}
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: "#3498db",
+        }}
       >
-        <Ionicons name="menu" size={24} color="black" />
-      </TouchableOpacity>
+        <View
+          style={{
+            backgroundColor: "#3498db",
+            marginTop: 50,
+            marginBottom: 16,
+            paddingLeft: 40,
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity onPress={openDrawer}>
+            <Ionicons name="menu" size={24} color="white" />
+          </TouchableOpacity>
+          <Text
+            style={{
+              paddingLeft: 70,
+              fontSize: 18,
+              fontWeight: "bold",
+              color: "white",
+            }}
+          >
+            Medical Record
+          </Text>
+        </View>
+      </View>
 
-      <Text>Hi, I am the patient page</Text>
-      <TouchableOpacity
-        style={{ position: "absolute", top: 60, right: 40 }}
-        onPress={logout}
-      >
-        <MaterialCommunityIcons name="logout" size={24} color="black" />
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <FlatList
+          key={"?"}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.index}
+          numColumns={2} // Number of columns in the grindex
+        />
+      </View>
       {drawerOpen && (
         <Animated.View style={[styles.drawer, { transform: [{ translateX }] }]}>
           <ScrollView>
@@ -120,24 +229,76 @@ export default function PatientPage({ navigation, route }) {
             <View style={styles.drawerHeader}>
               <Ionicons
                 name="md-person-circle-outline"
-                size={60}
+                size={65}
                 color="white"
               />
               <Text style={styles.patientName}>{patientName}</Text>
+            </View>
+            <View style={{ width: 300, height: 1, backgroundColor: "white" }}>
+              <Text></Text>
+            </View>
+            <View style={styles.drawerOptions}>
+              <TouchableOpacity
+                style={{ flexDirection: "row", paddingBottom: 15 }}
+                onPress={() => {
+                  navigation.navigate("Settings");
+                }}
+              >
+                <SimpleLineIcons name="settings" size={17} color="white" />
+                <Text style={styles.optionText}>Settings</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ flexDirection: "row", paddingBottom: 15 }}
+                onPress={() => {
+                  navigation.navigate("AccountInfo");
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="account"
+                  size={17}
+                  color="white"
+                />
+                <Text style={styles.optionText}>Account Information</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ flexDirection: "row", paddingBottom: 15 }}
+                onPress={() => {
+                  navigation.navigate("AboutUs");
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="progress-question"
+                  size={17}
+                  color="white"
+                />
+                <Text style={styles.optionText}>About Us</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{ flexDirection: "row" }}
+                onPress={() => {
+                  navigation.navigate("Privacy");
+                }}
+              >
+                <MaterialIcons name="privacy-tip" size={17} color="white" />
+                <Text style={styles.optionText}>Privacy Policy</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  marginTop: 470,
+                  flexDirection: "row",
+                }}
+                onPress={logout}
+              >
+                <SimpleLineIcons name="logout" size={24} color="white" />
+                <Text style={styles.optionText}>Logout</Text>
+              </TouchableOpacity>
             </View>
           </ScrollView>
         </Animated.View>
       )}
       {drawerOpen && (
         <TouchableOpacity
-          style={{
-            position: "absolute",
-            top: 0,
-            left: "60%",
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0,0,0)",
-          }}
+          style={styles.drawerCloseArea}
           onPress={closeDrawer}
         />
       )}
--- styles/patientStyles/patientPageStyle.js
+++ styles/patientStyles/patientPageStyle.js
@@ -8,14 +8,14 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "#4169E1",
+    backgroundColor: "#34495e",
     width: Dimensions.get("window").width * 0.6,
+    opacity: 0.9,
   },
   backButton: {
     position: "absolute",
     paddingHorizontal: 20,
     paddingVertical: 10,
-    borderRadius: 5,
     marginBottom: 10,
     marginTop: 30,
   },
@@ -27,9 +27,54 @@ const styles = StyleSheet.create({
   },
 
   drawerHeader: {
-    marginTop: 100,
-    alignItems: "center",
+    marginTop: 80,
+    padding: 10,
+    alignItems: "flex-start",
+    marginLeft: 10,
     marginBottom: 20,
+  },
+  drawerOptions: {
+    marginTop: 20,
+    marginLeft: 15,
+  },
+  optionText: {
+    color: "white",
+    paddingLeft: 5,
+  },
+  drawerCloseArea: {
+    position: "absolute",
+    top: 0,
+    left: "60%",
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0,0,0)",
+  },
+  item: {
+    backgroundColor: "#3498db",
+    padding: 80,
+    margin: 8,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    height: 140,
+    width: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  title: {
+    fontSize: 14,
+    position: "absolute",
+    bottom: 5,
+    color: "white",
+  },
+  image: {
+    height: 100,
+    width: 100,
+    marginBottom: 10,
+  },
+  container: {
+    top: 130,
   },
 });
 export default styles;
